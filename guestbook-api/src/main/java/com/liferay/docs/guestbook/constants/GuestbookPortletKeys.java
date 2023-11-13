5eb7399c60c868754201bf120251e4ef2306b97a
ddidenko <d.didenko@nicetu.spb.ru>
2023-11-13 15:58:27 +0300

    initial_commit_2

---
+++ guestbook-api/src/main/java/com/liferay/docs/guestbook/constants/GuestbookPortletKeys.java
@@ -0,0 +1,13 @@
+package com.liferay.docs.guestbook.constants;
+
+/**
+ * @author ddidenko
+ */
+public class GuestbookPortletKeys {
+
+	public static final String GUESTBOOK = "guestbook";
+	
+	public static final String GUESTBOOK_ADMIN =
+			  "com_liferay_docs_guestbook_portlet_GuestbookAdminPortlet";
+
+}
